2c1be358cf90ef331132c17d562b4ab81ebb06f8
Karim <limsu_@naver.com>
2022-06-17 22:20:12 +0900

    feat: 0.0.1-SNAPSHOT
    [postMapping test class 구현]
    - postMapping test class 구현
    
    BREAKING CHANGE: 2022-06-17
    - PostTestController.java 구현

---
+++ src/main/java/com/karim/restApiServer/controller/DbApiController.java
@@ -0,0 +1,9 @@
+package com.karim.restApiServer.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DbApiController {
+
+}
---
+++ src/main/java/com/karim/restApiServer/controller/PostTestController.java
@@ -0,0 +1,26 @@
+package com.karim.restApiServer.controller;
+
+import com.google.gson.JsonObject;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+// curl -X POST http://127.0.0.1:8080/api/v1/category/action?timeout=1 -d "{\"key\":\"value\"}" -H "Content-Type:application/json;charset=UTF-8"
+@RestController
+@RequestMapping("/api/v1")
+public class PostTestController {
+
+    @PostMapping(path = "/{category}/{action}/**", produces = "application/json;charset=UTF-8")
+    public String post(@PathVariable("category") String category,
+                           @PathVariable("action") String action,
+                           HttpSession httpSession,
+                           @RequestParam(value = "timeout") String timeout,
+                           @RequestBody String body) {
+        System.out.println(category);
+        System.out.println(action);
+        System.out.println(body);
+        System.out.println(httpSession);
+        System.out.println(timeout);
+        return new JsonObject().toString();
+    }
+}
